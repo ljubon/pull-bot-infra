0903a9964aa975e91081ffb99d275dac240fbcc0
Ljubo Nikolić <ljubonikolic@gmail.com>
2024-07-29 10:42:19 +0200

    use gross-devops-dev config on main

--- main.go
+++ main.go
@@ -7,30 +7,42 @@ import (
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ec2"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ecs"
 	"github.com/pulumi/pulumi-awsx/sdk/go/awsx/awsx"
-	awsxEcs "github.com/pulumi/pulumi-awsx/sdk/go/awsx/ecs"
+	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/cloudwatch"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	awsxEcs "github.com/pulumi/pulumi-awsx/sdk/go/awsx/ecs"
 )
 
 const (
-	PULL_CONTAINER  = "ghcr.io/ljubon/pull/pull:latest"
-	BUCKET          = "arn:aws:s3:::pullbot-envs/.env"
-	PRIVATE_KEY_ARN = "arn:aws:secretsmanager:us-east-1:341894770476:secret:LJUBOOPS_PRIVATE_KEY_ORIGINAL-LqUO7g" // ljuboops private key for github app
+	AWS_REGION			= "us-east-1"
+	TAG_COST_VALUE	= "d-server-01068mdjl5jze3"
+	BUCKET          = "arn:aws:s3:::pullbot-envs/.env" 
+
+	CLOUD_WATCH_GROUP = "service-1"
+
 	TASK_ROLE_ARN   = "arn:aws:iam::341894770476:role/ecsTaskExecutionRole"
-	TASK_ROLE_NAME  = "ecsTaskExecutionRole"
+	
 	ECS_ROLE_ARN    = "arn:aws:iam::341894770476:instance-profile/ecsInstanceRole"
-	ECS_ROLE_NAME   = "ecsInstanceRole"
-	VPC_ID          = "vpc-0fbca88fc6fab7a0f"
-	SECURITY_GROUP  = "sg-01a8e31f04b83e53d"
+	
 	CLUSTER_NAME    = "pull-pulumi-cluster"
 	SERVICE_NAME    = "service"
+
+	PULL_CONTAINER 		= "ghcr.io/ljubon/pull/pull:latest"
+	// Private key generated from github app
+	PRIVATE_KEY_ARN 	= "arn:aws:secretsmanager:us-east-1:341894770476:secret:LJUBOOPS_PRIVATE_KEY_ORIGINAL-LqUO7g"
+	
+	AMI_ID						= "ami-0e771da97cb597c23"
+	EC2_PRIVATE_KEY 	= "pullbot"
+	INSTANCE_TYPE			= "t2.medium"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		tags := pulumi.StringMap{
-			"map-migrated": pulumi.String("d-server-01068mdjl5jze3"), // This is required for INFRA team to manage costs
+			// This is required for INFRA team to manage costs
+			"map-migrated": pulumi.String(TAG_COST_VALUE),
 		}
 
+		// Create user data for EC2 instance, which will join the ECS cluster
 		encodedUserData := pulumi.All("pull-pulumi-cluster").ApplyT(func(args []interface{}) (string, error) {
 			userData := "#!bin/bash\necho ECS_CLUSTER=pull-pulumi-cluster >> /etc/ecs/ecs.config;"
 			return base64.StdEncoding.EncodeToString([]byte(userData)), nil
@@ -39,13 +51,10 @@ func main() {
 		// Create Launch template for EC2 instance for our cluster
 		launchTemplate, err := ec2.NewLaunchTemplate(ctx, "pull-pulumi-launch-template", &ec2.LaunchTemplateArgs{
 			Name:         pulumi.String("pull-pulumi-launch-template"),
-			ImageId:      pulumi.String("ami-0e771da97cb597c23"), // amzn2-ami-ecs-hvm-2.0.20240227-x86_64-ebs
-			InstanceType: pulumi.String("t2.medium"),
+			ImageId:      pulumi.String(AMI_ID),
+			InstanceType: pulumi.String(INSTANCE_TYPE),
 			UserData:     encodedUserData,
-			KeyName:      pulumi.String("pullbot"),
-			VpcSecurityGroupIds: pulumi.StringArray{
-				pulumi.String(SECURITY_GROUP),
-			},
+			KeyName:      pulumi.String(EC2_PRIVATE_KEY),
 			IamInstanceProfile: &ec2.LaunchTemplateIamInstanceProfileArgs{
 				Arn: pulumi.String(ECS_ROLE_ARN),
 			},
@@ -74,6 +83,12 @@ func main() {
 			return err
 		}
 
+		// Create a new CloudWatch Log Group
+		cloudwatch.NewLogGroup(ctx, CLOUD_WATCH_GROUP, &cloudwatch.LogGroupArgs{
+				Name: pulumi.String(CLOUD_WATCH_GROUP),
+				RetentionInDays: pulumi.Int(7),
+		})
+
 		/*
 			Creates a service which will deploy `DesiredCount` number of services with our container
 			NOTE: Make sure to change `v1` to next version when changing something of this service
@@ -85,26 +100,21 @@ func main() {
 			Name:         pulumi.String(SERVICE_NAME),
 			Cluster:      cluster.Arn,
 			DesiredCount: pulumi.Int(1),
-			NetworkConfiguration: ecs.ServiceNetworkConfigurationArgs{
-				SecurityGroups: pulumi.StringArray{
-					pulumi.String(SECURITY_GROUP),
-				},
-				Subnets: pulumi.StringArray{
-					// When fresh deploying to new region/account add created subnets
-					pulumi.String("subnet-02c6606e6327a2524"),
-					pulumi.String("subnet-0e8a610a8547bd5a1"),
-					pulumi.String("subnet-026cf2674f7b9e008"),
-					pulumi.String("subnet-06999ccd7f8a4d538"),
-					pulumi.String("subnet-0a278c1c001e0608e"),
-					pulumi.String("subnet-0ac5a47ac46d2a3d8"),
-				},
-			},
 			TaskDefinitionArgs: &awsxEcs.EC2ServiceTaskDefinitionArgs{
+				NetworkMode: pulumi.String("host"),
 				Container: &awsxEcs.TaskDefinitionContainerDefinitionArgs{
 					Image:     pulumi.String(PULL_CONTAINER),
 					Cpu:       pulumi.Int(1024),
 					Memory:    pulumi.Int(2048),
 					Essential: pulumi.Bool(true),
+					LogConfiguration: &awsxEcs.TaskDefinitionLogConfigurationArgs{
+						LogDriver: pulumi.String("awslogs"),
+						Options: pulumi.StringMap{
+							"awslogs-group":         pulumi.String(CLOUD_WATCH_GROUP),
+							"awslogs-region":        pulumi.String(AWS_REGION),
+							"awslogs-stream-prefix": pulumi.String("container"),
+						},
+					},
 					Secrets: awsxEcs.TaskDefinitionSecretArray{
 						awsxEcs.TaskDefinitionSecretArgs{
 							Name:      pulumi.String("PRIVATE_KEY"),
@@ -125,8 +135,6 @@ func main() {
 						},
 					},
 				},
-				// NOTE: When deploying fresh infra, deploy with commenting this line, after deploy enable this mode
-				// NetworkMode: pulumi.String("host"),
 
 				ExecutionRole: &awsx.DefaultRoleWithPolicyArgs{
 					RoleArn: pulumi.String(TASK_ROLE_ARN),
